20e6efcf0ffe61e83b6aff1990c69471be741b95
Eshap <g.eshap09@gmail.com>
2020-08-07 16:19:45 +0530

    Updates

---
+++ precache-manifest.c34cfb54622c9fdcdc45cf8a6aa8e82d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43075ceeed54cd6f62f9e511205e1be0",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "0154168a3627813916f2",
+    "url": "/react-todolist/static/css/main.1772931e.chunk.css"
+  },
+  {
+    "revision": "22d17d1a0e365fa09123",
+    "url": "/react-todolist/static/js/2.fe68eac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.fe68eac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0154168a3627813916f2",
+    "url": "/react-todolist/static/js/main.9ef97e6b.chunk.js"
+  },
+  {
+    "revision": "dea162c310a378ff9d0d",
+    "url": "/react-todolist/static/js/runtime-main.82f4965a.js"
+  }
+]);
